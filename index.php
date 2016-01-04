0a93a2b1412758c3ee4d1a8fe8f2ea07a6ddfded
victordieggo <victordieggo@gmail.com>
2016-01-04 12:23:16 -0200

    Versão 1.7.5
    -Classe .grid substituida por .row
    
    -Definidos novos breakpoints: 992px, 768px e 480px
    
    -Font Awesome atualizado para versão 4.5
    
    -Flexslider atualizado para versão 2.6.0
    
    -Várias Helper Classes/Classes de ajuda implementadas:
     -.pad-# e .spacer-# (números pulam de 5 e 5, começando em 0.5em até 3.0em, com opções apenas para top ou bottom)
     -.hide, .show: opções para cada breakpoint: -md, -sm, -ex-sm
     -.full-width, .auto-width, .auto-height e .auto-size
     -.no-spacer: colunas sem margin-top e margin-bottom, utilizável em header-top e etc (classe APENAS para colunas do grid)
     -.no-underline: links sem underline no hover
    
    -Implementado menu lateral a partir da resolução média
    
    -CSS fracionado em grid.css, typography.css, base.css e layout.css

--- index.php
+++ index.php
@@ -2,140 +2,154 @@
 <html lang="pt-br">
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Spaceman Framework</title>
-	<link rel="icon" href="img/icon.png" />
-	<link rel="stylesheet" href="css/global-styles.css" type="text/css" media="screen" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<meta name="description" content="">
-	<meta name="keywords" content="">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Spaceman Framework 1.7.5</title>
+    <link rel="icon" href="img/icon.png" />
+    <link rel="stylesheet" href="css/global-styles.css" type="text/css" media="screen" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
 </head>
 
-<body>
+<body class="bg-grey">
 
-	<!-- BEGIN HEADER-TOP -->
-	<div class="bg-dark">
-		<div class="wrapper">
-			<div class="grid txt-white small">
-				<div class="sixcol">
-					<p>Version 1.7</p>
-				</div>
-				<div class="sixcol align-right">
-					<p>I'm gonna chase you out of earth!</p>
-				</div>
+    <!-- BEGIN HEADER-TOP -->
+    <div class="bg-dark hide-sm">
+        <div class="wrapper">
+            <div class="row txt-white small">
+                <div class="sixcol no-spacer">
+                    <p>Version 1.7.5</p>
+                </div>
+                <div class="sixcol align-right no-spacer">
+                    <p>I'm gonna chase you out of earth!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END HEADER TOP-->
+
+    <!-- BEGIN HEADER -->
+    <header class="wrapper bg-primary">
+        <div class="row">
+            <div class="twelvecol">
+
+                <a class="logo fleft vcenter" href=""></a>
+				
+                <!-- BEGIN NAVIGATION -->
+                <nav class="main-nav vcenter hide-md">
+                    <ul class="menu">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                        <li><a href="#">Serviços</a></li>
+                        <li><a href="#">Fale Conosco</a></li>
+                    </ul>
+                </nav>
+                <!-- END NAVIGATION -->
+				
+                <button class="nav-btn bg-main show-md fright">
+                    <i class="fa fa-bars fa-2x txt-purple"></i>
+                </button>
+
+            </div>
+        </div>
+    </header>
+    <!-- END HEADER -->
+
+    <!-- BEGIN SLIDER -->
+    <div class="wrapper">
+        <div class="flexslider">
+            <ul class="slides">
+                <li><img src="img/slide1.jpg" alt=""></li>
+                <li><img src="img/slide2.jpg" alt=""></li>
+            </ul>
+        </div>
+    </div>
+    <!-- END SLIDER -->
+
+    <!-- BEGIN CONTENT -->
+    <section class="wrapper">
+        <div class="row bg-white">
+			
+            <div class="fourcol stretch">
+                <img src="img/thumb.jpg" alt="thumb">
+                <h3>Lorem Ipsum</h3>
+                <hr class="spacer-10">
+                <p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket. Bresaola shankle leberkas t-bone strip steak, capicola chuck hamburger bacon pork meatloaf ball...</p>
+            </div>
+			
+            <div class="fourcol stretch">
+                <img src="img/thumb.jpg" alt="thumb">
+                <h3>Lorem Ipsum</h3>
+                <hr class="spacer-10">
+                <p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket. Bresaola shankle leberkas t-bone strip steak, capicola chuck hamburger bacon pork meatloaf ball...</p>
+            </div>
+			
+            <div class="fourcol stretch">
+                <img src="img/thumb.jpg" alt="thumb">
+                <h3>Lorem Ipsum</h3>
+                <hr class="spacer-10">
+                <p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket. Bresaola shankle leberkas t-bone strip steak, capicola chuck hamburger bacon pork meatloaf ball...</p>
+            </div>
+			
+        </div>
+    </section>
+    <!-- END CONTENT -->
+
+    <!-- BEGIN FOOTER -->
+    <footer>
+		
+        <div class="wrapper bg-dark txt-white">
+            <div class="row">
+
+                <div class="threecol stretch-mid">
+                    <h5>Lorem Ipsum</h5>
+                    <hr class="spacer-10">
+                    <p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket...</p>
+                </div>
+
+                <div class="threecol stretch-mid">
+                    <h5>Lorem Ipsum</h5>
+                    <hr class="spacer-10">
+                    <p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket...</p>
+                </div>
+
+                <div class="threecol stretch-mid">
+                    <h5>Lorem Ipsum</h5>
+                    <hr class="spacer-10">
+                    <p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket...</p>
+                </div>
+
+                <div class="threecol stretch-mid">
+                    <h5>Lorem Ipsum</h5>
+                    <hr class="spacer-10">
+                    <p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket...</p>
+                </div>
+				
 			</div>
 		</div>
-	</div>
-	<!-- END HEADER TOP-->
+		
+		<div class="bg-darker">
+            <div class="row txt-white">
+                <div class="twelvecol pad-05 align-center small no-spacer">
+                    <p>© 2015 Spaceman 1.7.5 Framework. | Desenvolvido por <a class="no-underline" href="">V. de Várias Fita</a>.</p>
+                </div>
+            </div>
+        </div>
+		
+    </footer>
+    <!-- END FOOTER -->
 
-	<!-- BEGIN HEADER -->
-	<header class="wrapper bg-main">
-		<div class="grid">
-			<div class="sixcol">
-				<a class="logo" href=""></a>
-			</div>
-
-			<!-- BEGIN NAVIGATION -->
-			<div class="sixcol">
-				<nav>
-					<div class="mobile-nav">Menu</div>
-					<ul class="menu">
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Sobre</a></li>
-						<li><a href="#">Serviços</a></li>
-						<li><a href="#">Fale Conosco</a></li>
-					</ul>
-				</nav>
-			</div>
-			<!-- END NAVIGATION -->
-
-		</div>
-	</header>
-	<!-- END HEADER -->
-
-	<!-- BEGIN SLIDER -->
-	<div class="wrapper">
-		<div class="flexslider">
-			<ul class="slides">
-				<li><img src="img/slide1.jpg" alt="" /></li>
-			</ul>
-		</div>
-	</div>
-	<!-- END SLIDER -->
-
-	<!-- BEGIN CONTENT -->
-	<div class="wrapper">
-		<div class="grid bg-light">			
-			<div class="fourcol">
-				<img src="img/thumb.jpg" alt="thumb" />
-				<h3>Lorem Ipsum</h3>
-				<hr/>
-				<p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket. Bresaola shankle leberkas t-bone strip steak, capicola chuck hamburger bacon pork meatloaf ball...</p>
-			</div>
-			<div class="fourcol">
-				<img src="img/thumb.jpg" alt="thumb" />
-				<h3>Lorem Ipsum</h3>
-				<hr/>
-				<p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket. Bresaola shankle leberkas t-bone strip steak, capicola chuck hamburger bacon pork meatloaf ball...</p>
-			</div>
-			<div class="fourcol">
-				<img src="img/thumb.jpg" alt="thumb" />
-				<h3>Lorem Ipsum</h3>
-				<hr/>
-				<p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket. Bresaola shankle leberkas t-bone strip steak, capicola chuck hamburger bacon pork meatloaf ball...</p>
-			</div>
-		</div>
-	</div>
-	<!-- END CONTENT -->
-
-	<!-- BEGIN FOOTER -->
-	<footer>
-		<div class="wrapper bg-dark txt-white">
-			<div class="grid">
-				<div class="threecol">
-					<h5>Lorem Ipsum</h5>
-					<hr/>
-					<p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket...</p>
-				</div>
-				<div class="threecol">
-					<h5>Lorem Ipsum</h5>
-					<hr/>
-					<p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket...</p>
-				</div>
-				<div class="threecol">
-					<h5>Lorem Ipsum</h5>
-					<hr/>
-					<p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket...</p>
-				</div>
-				<div class="threecol">
-					<h5>Lorem Ipsum</h5>
-					<hr/>
-					<p>Bacon ipsum dolor amet strip steak flank beef pastrami tail, shoulder kielbasa tenderloin fatback brisket...</p>
-				</div>
-			</div>
-		</div>
-	</footer>
-
-	<div class="bg-darker">
-		<div class="grid txt-white">
-			<div class="twelvecol align-center small">
-				<p>© 2015 Spaceman 1.7 Framework. | Desenvolvido por V. de Várias Fita.</p>
-			</div>
-		</div>
-	</div>
-	<!-- END FOOTER -->
-
-	<!-- BEGIN JQUERY // PLACED AT THE BOTTOM OF THE PAGE FOR FASTER LOADING -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script src="js/global-js.js"></script>
-	<script type="text/javascript">
-		$(window).load(function () {
-			$('.flexslider').flexslider({
-				controlNav: false,
-			});
-		});
-	</script>
-	<!-- END JQUERY -->
+    <!-- BEGIN JQUERY // PLACED AT THE BOTTOM OF THE PAGE FOR FASTER LOADING -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="js/global-js.js"></script>
+    <script type="text/javascript">
+        $(window).load(function () {
+            $('.flexslider').flexslider({
+                controlNav: false,
+            });
+        });
+    </script>
+    <!-- END JQUERY -->
 
 </body>
 </html>
--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,41 +1,69 @@
-// MULTILEVEL MOBILE NAVIGATION
-var menu = $('ul.menu');
-$(document).ready(function(){
+jQuery(document).ready(function(){
 	
 	//ABRE O MENU
-	$('.mobile-nav').click(function() {
-		if ($(window).width() <= 1023) {
-			menu.slideToggle(400);
+	jQuery('.nav-btn').click(function() {
+		if (jQuery(window).width() <= 1023) {
+			jQuery('.main-nav').animate({width:'toggle'},250);
 		}
 	});
 	
 	//ABRE O SUBMENU
-	$('.menu-item-has-children').click(function(){
-		if ($(window).width() <= 1023) {
-			$(this).children('.sub-menu').slideToggle(400);
+	jQuery('.menu-item-has-children').click(function(){
+		if (jQuery(window).width() <= 1023) {
+			jQuery(this).children('.sub-menu').slideToggle(400);
+			
+			//ADD HIGHLIGHT COLOR
+			jQuery(this).children('a').toggleClass('nav-highlight');
 			
 			//EVITA QUE TODAS OS NÍVEIS DO SUBMENU SEJAM ABERTOS JUNTOS
-			$('.sub-menu').children().click(function (event) {
+			jQuery('.sub-menu').children().click(function (event) {
 				event.stopPropagation();
 			});
 		}
 	});
+	
+	//CLOSE MENU IF USER CLICKS OUTSIDE
+    jQuery('html').click(function() {
+		if (jQuery(window).width() <= 992) {
+			if (jQuery('.main-nav').is(':visible') ){
+				jQuery('.main-nav').animate({width:'toggle'},250);
+				jQuery('.nav-btn').removeClass( "selected" );
+			} 
+		}
+    });
+    jQuery('.main-nav, .nav-btn').click(function(event){
+        event.stopPropagation();
+    });
+	
 });
 		
-//EVITA QUE O MENU OU SUBMENU PERMANEÇAM ABERTOS
-$(window).on('resize', function() {
-	if (!$(".mobile-nav").is(":visible") && !menu.is(':visible')) {
-		menu.css({
+//RESIZE FALLBACK
+jQuery(window).on('resize', function() {
+	if (jQuery(window).width() >= 993) {
+		
+		//FORCE MAIN NAV TO APPEAR EVEN AFTER BEIGN CLOSED ON MOBILE
+		if (jQuery('.main-nav').not(':visible')) {
+			jQuery('.main-nav').css({
+			'display': ''
+			});
+		}
+		
+		//FORCE SUB-MENU ITENS TO HIDE IF THEY ARE OPEN ON MOBILE
+		if (jQuery('ul.sub-menu').is(':visible')) {
+		jQuery('ul.sub-menu').css({
 			'display': ''
 		});
-	}
-	if ($('ul.sub-menu').is(':visible')) {
-		$('ul.sub-menu').css({
-			'display': ''
-		});
-	} else {
-		$('ul.sub-menu').css({
-			'display': ''
-		});
+		} else {
+			jQuery('ul.sub-menu').css({
+				'display': ''
+			});
+		}
+		
+		//REMOVE HIGHLIGHT COLOR
+		jQuery('.menu-item-has-children').children('a').removeClass('nav-highlight');
+		
+		//REMOVE SELECTED CLASS FROM NAV BUTTON
+		jQuery('.nav-btn').removeClass( "selected" );
+		
 	}
 });
