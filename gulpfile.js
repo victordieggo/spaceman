bf0df5924dc581aecdcb060ff27f8dcdc8eaea12
Victor Diego <victordieggo@gmail.com>
2017-11-07 16:02:41 -0200

    Fix indent

--- gulpfile.js
+++ gulpfile.js
@@ -1,123 +1,98 @@
-/* =====================================================================
- * GULPFILE FOR SPACEMAN
- * ===================================================================*/
-/*jslint node: true */
 'use strict';
 
-//-------------------------------------------------------------------
-// SET VARIABLES
-//-------------------------------------------------------------------
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var stylelint = require('gulp-stylelint');
+var combineMq = require('gulp-combine-mq');
+var cssmin = require('gulp-cssmin');
+var postcss = require('gulp-postcss');
+var eslint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var mozjpeg = require('imagemin-mozjpeg');
+var pngquant = require('imagemin-pngquant');
+var browserSync = require('browser-sync').create();
+var path = require('path');
 
-var browserSync = require('browser-sync').create(),
-    gulp        = require('gulp'),
-    combineMq   = require('gulp-combine-mq'),
-    concat      = require('gulp-concat'),
-    cssmin      = require('gulp-cssmin'),
-    eslint      = require('gulp-eslint'),
-    imagemin    = require('gulp-imagemin'),
-    postcss     = require('gulp-postcss'),
-    stylelint   = require('gulp-stylelint'),
-    uglify      = require('gulp-uglify'),
-    mozjpeg     = require('imagemin-mozjpeg'),
-    pngquant    = require('imagemin-pngquant'),
-    cssNext     = require('postcss-cssnext'),
-    precss      = require('precss'),
-    path        = require('path'),
-    basePath = {
-        src:  'assets/src/',
-        dist: 'assets/dist/'
-    },
-    srcPath = {
-        cfg: basePath.src + 'css/config.css',
-        css: basePath.src + 'css/!(config.css)*.css',
-        js:  basePath.src + 'js/*.js',
-        img: basePath.src + 'img/*.{png,gif,jpg}',
-        svg: basePath.src + 'svg/*.svg',
-    },
-    distPath = {
-        css: basePath.dist + 'css',
-        js:  basePath.dist + 'js',
-        img: basePath.dist + 'img',
-        svg: basePath.dist + 'svg',
-    },
-    bsReload = ['./**/*.{html,php}', srcPath.svg];
+var postcssPlugins = [
+  require('precss'),
+  require('postcss-cssnext')
+];
 
-//-------------------------------------------------------------------
-// BUILD CSS
-//-------------------------------------------------------------------
+var basePath = {
+  src:  'assets/src/',
+  dist: 'assets/dist/'
+};
+
+var srcPath = {
+  cfg: basePath.src + 'css/config.css',
+  css: basePath.src + 'css/*.css',
+  js:  basePath.src + 'js/*.js',
+  img: basePath.src + 'img/*.{png,gif,jpg}',
+  svg: basePath.src + 'svg/*.svg',
+};
+
+var distPath = {
+  css: basePath.dist + 'css',
+  js:  basePath.dist + 'js',
+  img: basePath.dist + 'img',
+  svg: basePath.dist + 'svg',
+};
+
+var bsReload = ['./**/*.{html,php}', srcPath.svg];
 
 gulp.task('css', function () {
-    var plugins = [
-        precss(),
-        cssNext()
-    ];
-    gulp.src([srcPath.cfg, srcPath.css])
-        .pipe(stylelint({
-            failAfterError: false,
-            reporters: [
-                {formatter: 'string', console: true}
-            ]
-        }))
-        .pipe(concat('style.css'))
-        .pipe(postcss(plugins))
-        .pipe(combineMq())
-        .pipe(cssmin())
-        .pipe(gulp.dest(distPath.css))
-        .pipe(browserSync.stream());
+  gulp.src([srcPath.cfg, srcPath.css])
+    .pipe(stylelint({
+      failAfterError: false,
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }))
+    .pipe(concat('style.css'))
+    .pipe(postcss(postcssPlugins))
+    .pipe(combineMq())
+    .pipe(cssmin())
+    .pipe(gulp.dest(distPath.css))
+    .pipe(browserSync.stream());
 });
-
-//-------------------------------------------------------------------
-// BUILD JS
-//-------------------------------------------------------------------
 
 gulp.task('js', function () {
-    gulp.src(srcPath.js)
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(distPath.js))
-        .pipe(browserSync.stream());
+  gulp.src(srcPath.js)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(distPath.js))
+    .pipe(browserSync.stream());
 });
-
-//-------------------------------------------------------------------
-// MINIFY IMAGES
-//-------------------------------------------------------------------
 
 gulp.task('img', function () {
-    gulp.src(srcPath.img)
-        .pipe(imagemin([
-            mozjpeg({quality: 89}),
-            pngquant({quality: 70})
-        ], {verbose: true}))
-        .pipe(gulp.dest(distPath.img))
-        .pipe(browserSync.stream());
+  gulp.src(srcPath.img)
+    .pipe(imagemin([
+      mozjpeg({quality: 89}),
+      pngquant({quality: 70})
+    ], {verbose: true}))
+    .pipe(gulp.dest(distPath.img))
+    .pipe(browserSync.stream());
 });
-
-//-------------------------------------------------------------------
-// MINIFY SVG
-//-------------------------------------------------------------------
 
 gulp.task('svg', function () {
-    gulp.src(srcPath.svg)
-        .pipe(imagemin({verbose: true}))
-        .pipe(gulp.dest(distPath.svg));
+  gulp.src(srcPath.svg)
+    .pipe(imagemin({verbose: true}))
+    .pipe(gulp.dest(distPath.svg));
 });
 
-//-------------------------------------------------------------------
-// WATCH + DEFAULT
-//-------------------------------------------------------------------
-
 gulp.task('watch', function () {
-    browserSync.init({
-        proxy: 'localhost/' + path.basename(__dirname),
-        open: false,
-    });
-    gulp.watch(srcPath.js, ['js']);
-    gulp.watch(srcPath.css, ['css']);
-    gulp.watch(srcPath.img, ['img']);
-    gulp.watch(srcPath.svg, ['svg']);
-    gulp.watch(bsReload, browserSync.reload);
+  browserSync.init({
+    proxy: 'localhost/' + path.basename(__dirname),
+    open: false,
+  });
+  gulp.watch(srcPath.js, ['js']);
+  gulp.watch(srcPath.css, ['css']);
+  gulp.watch(srcPath.img, ['img']);
+  gulp.watch(srcPath.svg, ['svg']);
+  gulp.watch(bsReload, browserSync.reload);
 });
 
 gulp.task('default', ['js', 'css', 'img', 'svg', 'watch']);
