f1bb12058687ee1bb35758a9aa892221a9eba1a4
Victor Diego <victordieggo@gmail.com>
2018-01-27 14:01:09 -0200

    Update functions to arrow functions

--- assets/src/js/navigation.js
+++ assets/src/js/navigation.js
@@ -15,55 +15,54 @@
   const navBtn = nav.querySelector('.nav-btn');
   const parentItem = nav.querySelectorAll('.menu-item-has-children');
 
-  function toggleScroll() {
+  const toggleScroll = () => {
     body.classList.toggle('nav-hide-overflow');
     if (nav.contains(screenCover)) {
       nav.removeChild(screenCover);
     } else {
       screenCover = document.createElement('div');
       screenCover.className = 'screen-cover';
-      screenCover.onclick = function () {};
+      screenCover.onclick = () => {};
       nav.appendChild(screenCover);
     }
-  }
+  };
 
-  function toggleExpanded(element) {
+  const toggleExpanded = (element) => {
     const ariaExpanded = element.getAttribute('aria-expanded');
     element.setAttribute('aria-expanded', ariaExpanded == 'false' ? 'true' : 'false');
-  }
+  };
 
-  function toggleNavigation() {
+  const toggleNavigation = () => {
     nav.classList.toggle(navIsActive);
     toggleExpanded(navBtn);
     toggleScroll();
-  }
+  };
 
-  function toggleSubmenu(element) {
+  const toggleSubmenu = (element) => {
     const link = element.querySelector('a');
-    link.addEventListener('click', function (event) {
+    link.addEventListener('click', (event) => {
       event.preventDefault();
       element.classList.toggle(itemIsActive);
       toggleExpanded(link);
     });
-  }
+  };
 
-  Array.prototype.forEach.call(['click', 'keyup'], function (event) {
-    body.addEventListener(event, function (event) {
+  Array.prototype.forEach.call(['click', 'keyup'], (eventType) => {
+    body.addEventListener(eventType, (event) => {
       const type = event.type;
       const key = event.keyCode;
       const target = event.target;
-      const onFocus = nav.querySelector(':focus');
       const activeItems = nav.querySelectorAll('.' + itemIsActive);
-      function closeActiveItem(activeItem) {
+      const closeActiveItem = (activeItem) => {
         activeItem.classList.remove(itemIsActive);
         toggleExpanded(activeItem.querySelector('a'));
-      }
+      };
       if (type == 'click') {
         if (target == navBtn || target == screenCover) {
           toggleNavigation();
         }
       }
-      Array.prototype.forEach.call(activeItems, function (activeItem) {
+      Array.prototype.forEach.call(activeItems, (activeItem) => {
         if (type == 'click' && !activeItem.contains(target)) {
           const activeNav = nav.contains(screenCover);
           const browserWidth = window.innerWidth;
@@ -73,12 +72,12 @@
             navBtn
           ];
           exceptions = exceptions.indexOf(target) == -1;
-          if (browserWidth > mediumViewport || (browserWidth <= mediumViewport && exceptions && activeNav)) {
+          if (browserWidth > mediumViewport || browserWidth <= mediumViewport && exceptions && activeNav) {
             closeActiveItem(activeItem);
           }
         }
         if (type == 'keyup') {
-          if (key == 27 || (key == 9 && !activeItem.contains(onFocus))) {
+          if (key == 27 || key == 9 && !activeItem.contains(nav.querySelector(':focus'))) {
             closeActiveItem(activeItem);
           }
         }
@@ -86,8 +85,6 @@
     });
   });
 
-  Array.prototype.forEach.call(parentItem, function (element) {
-    toggleSubmenu(element);
-  });
+  Array.prototype.forEach.call(parentItem, (element) => toggleSubmenu(element));
 
 }());
